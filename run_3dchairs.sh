56e94f349ef56e3709a2b3945036d1385ef89df1
1Konny <lee.wonkwang94@gmail.com>
2018-04-17 12:10:55 +0900

    edit run parameters

--- run_3dchairs.sh
+++ run_3dchairs.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python main.py --dataset 3dchairs --beta 5 --lr 1e-4 --z_dim 32 --viz_name beta_vae_3dchairs --viz_port 55558
+python main.py --dataset 3dchairs --beta 4 --lr 1e-4 --z_dim 10 --viz_name beta_vae_3dchairs --viz_port 55558
--- run_celeba.sh
+++ run_celeba.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python main.py --dataset celeba --beta 250 --lr 1e-4 --z_dim 32 --viz_name beta_vae_celeba --viz_port 55558
+python main.py --dataset celeba --beta 64 --lr 1e-4 --z_dim 10 --viz_name beta_vae_celeba --viz_port 55558
